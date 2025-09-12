1c7f0c16a7629e2a40d097af64127b27efdaa33e
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-12 16:47:21 +0500

    Add Landing page

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Landing.css'
+
+const Landing = () => {
+  return (
+    <>
+      <section class="landing">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>Frontend Developer | React | Web Designer</p>
+        <a href="#" class="btn">Explore My Work</a>
+      </section>
+    </>
+  )
+}
+
+export default Landing
