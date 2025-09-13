d053c50a74c62ffd09f9b637dcd58a3859fa335e
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-13 14:00:26 +0500

    Add content

---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const content=[
+    {
+        about :"A passionate Front-End Web Developer with expertise in HTML, CSS, JavaScript, and React.js. I love creating clean, responsive, and user-friendly web interfaces that provide a great experience for users. I enjoy turning ideas into functional websites and constantly learning new technologies to improve my skills in web development."
+    }
+]
