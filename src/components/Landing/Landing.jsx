d5dac313174a451d2beccdb1f7cd53895990c61f
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-13 14:01:16 +0500

    Add profile image

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import './Landing.css'
+import {content } from '../../data.js'
+import img from "../../assets/software-developer.png";
 
 const Landing = () => {
   return (
-    <>
-      <section class="landing">
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <h1>Welcome to My Portfolio</h1>
+    <section className="landing">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="landing-left">
+        <h1 className="typing">Hi! I’m Ateeb</h1>
+
         <p>Frontend Developer | React | Web Designer</p>
-        <a href="#" class="btn">Explore My Work</a>
-      </section>
-    </>
+        <p>{content[0].about}</p>
+        <div className='button-container'>
+          <a href="#" className="btn">Explore My Work</a>
+          <a href="#" className="btn">Download Resume</a>
+        </div>
+      </div>
+      <div className="landing-right">
+        <img src={img} alt="Ateeb"/>
+      </div>
+    </section>
   )
 }
 
